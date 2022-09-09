4f355a3c6111e8f979efd84cbde9c21580249a5f
Egor-Shiryaev <euro24rus@gmail.com>
2022-09-09 14:19:06 +0700

    new file

--- js/Set the id of an Element copy.js
+++ js/Set the id of an Element copy.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// Create a function timesFive that accepts one argument,
+// multiplies it by 5, and returns the new value.
+
+function timesFive(a) {
+  return a * 5;
+}
+timesFive(3);

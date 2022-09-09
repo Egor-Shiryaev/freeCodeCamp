8bb639408903f0eb13f764032a973bb4d46da938
Egor-Shiryaev <euro24rus@gmail.com>
2022-09-09 14:37:26 +0700

    asd

--- js/Local Scope and Functions.js
+++ js/Local Scope and Functions.js
@@ -7,3 +7,13 @@
 // Note: The console will still display 
 // ReferenceError: myVar is not defined, but this will not cause the tests to fail
 
+function myLocalScope() {
+    // Only change code below this line
+  let myVar;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);

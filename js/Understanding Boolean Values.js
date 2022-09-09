dcc7f24ec2f99772274df0b02329bf8b96f08b6f
Egor-Shiryaev <euro24rus@gmail.com>
2022-09-09 21:47:09 +0700

    inite: new file

---
+++ js/Understanding Boolean Values.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Modify the welcomeToBooleans function so that it returns true
+// instead of false when the run button is clicked.
+
+function welcomeToBooleans() {
+  return true; // Change this line
+}

ebf26d8411ce7e8594ddaed5431bfc5ae2500fdb
Egor-Shiryaev <euro24rus@gmail.com>
2022-09-09 14:34:17 +0700

    asd

---
+++ js/Local Scope and Functions.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// The editor has two console.logs to help you see what is happening. 
+// Check the console as you code to see how it changes. 
+// Declare a local variable myVar inside myLocalScope and run the tests.
+
+// Note: The console will still display 
+// ReferenceError: myVar is not defined, but this will not cause the tests to fail
+
